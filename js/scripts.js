d74b31cb75a83efe0ea8827bd0c8d0aa7562a639
Davit Berulava <48175961+davit2003@users.noreply.github.com>
2022-02-23 02:17:05 +0200

    Pop-up

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,42 @@
 window.onscroll = function (e) {
     let sidebar = document.getElementsByClassName("sidebar")[0];
     let menu = document.getElementsByClassName("navbar")[0];
-    if(window.scrollY > 600){
-        sidebar.style.right="0px";
+    
+    if(window.scrollY > 80)
         menu.style.background = "#5d0068";
+    else
+        menu.style.background = "transparent";
+
+    if(window.scrollY > 600)
+        sidebar.style.right="0px";
+    else
+        sidebar.style.right="-40px";
+  };
+
+function openclose(obj){
+    i = obj.getElementsByClassName("fa-solid")[0];
+    if(i.classList.contains("fa-plus")){
+        i.classList.remove("fa-plus");
+        i.classList.add("fa-minus")
     }
     else{
-        sidebar.style.right="-40px";
-        menu.style.background = "transparent";
+        i.classList.remove("fa-minus")
+        i.classList.add("fa-plus");
     }
-  };
+}
+function modal(){
+    document.querySelector("#Modal").classList.add("show");
+    document.querySelector("#Modal").style.display= "block";
+}
+function modalClose(){
+    document.querySelector("#Modal").classList.add("show");
+    document.querySelector("#Modal").style.display= "none";
+
+}
+function comingsoon(obj){
+    
+    obj.children[0].innerHTML = "Coming Soon!";
+}
+function back(){
+
+}
