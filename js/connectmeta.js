fd1b55e7a8088d51351907b46037ac743d23f99f
Davit Berulava <48175961+davit2003@users.noreply.github.com>
2022-09-10 22:08:11 +0300

    Menu Change

--- js/connectmeta.js
+++ js/connectmeta.js
@@ -1,7 +1,7 @@
 async function connect() {
   if (window.ethereum) {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
+    // const account = accounts[0];
     //Get the current MetaMask selected/active wallet
     const walletAddress = ethereum.selectedAddress;
     console.log(`Wallet: ${walletAddress}`);
@@ -10,5 +10,12 @@ async function connect() {
     console.log("No wallet");
     window.web3 = new Web3(window.ethereum);
     const account = web3.eth.accounts;
+  }
+}
+window.onload = function () {
+  wallet = document.getElementById("connectWallet").firstChild;
+  if(!window.ethereum){
+    wallet.innerHTML = "Connected";
+    wallet.disabled;
   }
 }
