855c4ac08c4a51f973a159e635047059c22dbb16
CyberAngel33 <Tsl@Tslpantheon.com>
2022-09-10 22:47:11 +0300

    Havanaunana

--- js/connectmeta.js
+++ js/connectmeta.js
@@ -4,18 +4,28 @@ async function connect() {
     // const account = accounts[0];
     //Get the current MetaMask selected/active wallet
     const walletAddress = ethereum.selectedAddress;
-    console.log(`Wallet: ${walletAddress}`);
-
+    // console.log(`Wallet: ${walletAddress}`);
   } else {
     console.log("No wallet");
     window.web3 = new Web3(window.ethereum);
     const account = web3.eth.accounts;
   }
+  Change();
 }
 window.onload = function () {
-  wallet = document.getElementById("connectWallet").firstChild;
-  if(!window.ethereum){
-    wallet.innerHTML = "Connected";
-    wallet.disabled;
+  Change();
+}
+web3.ethereum.addEventListener("disconnect", ()=> {
+  Change()
+});
+function Change() {
+  let wall = document.getElementById("connectWall").firstElementChild;
+  if(ethereum.selectedAddress != null){
+    wall.innerHTML = "Connected";
+    document.getElementById("connectWall").firstElementChild.style.cursor = "not-allowed";
+  }
+  else{
+    wall.innerHTML = "Connect Wallet";
+    wall.style.cursor = "pointer";
   }
 }
