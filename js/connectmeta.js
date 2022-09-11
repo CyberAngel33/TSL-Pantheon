433f8f4f8337498f829b39ecafacdaca565fa2a0
CyberAngel33 <Tsl@Tslpantheon.com>
2022-09-11 22:51:11 +0300

    aba shen ici raindo, motyani kodi da dagindob

--- js/connectmeta.js
+++ js/connectmeta.js
@@ -1,33 +1,29 @@
-async function connect() {
-  if (window.ethereum) {
-    if(ethereum.walletAddress != null){
-      ethereum.walletAddress = null;
-    }
-    else{
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-      // const account = accounts[0];
-      //Get the current MetaMask selected/active wallet
-      const walletAddress = ethereum.selectedAddress;
-      // console.log(`Wallet: ${walletAddress}`);
-    }
-  } else {
-    console.log("No wallet");
-    window.web3 = new Web3(window.ethereum);
-    const account = web3.eth.accounts;
-  }
-  Change();
-}
-window.onload = function () {
-  Change();
-}
-function Change() {
-  let wall = document.getElementById("connectWall").firstElementChild;
-  if(ethereum.selectedAddress != null){
-    wall.innerHTML = "Connected";
-    document.getElementById("connectWall").firstElementChild.style.cursor = "not-allowed";
-  }
-  else{
-    wall.innerHTML = "Connect Wallet";
-    wall.style.cursor = "pointer";
-  }
-}
+// async function connect() {
+//   if (window.ethereum) {
+//     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+//     // const account = accounts[0];
+//     //Get the current MetaMask selected/active wallet
+//     const walletAddress = ethereum.selectedAddress;
+//     // console.log(`Wallet: ${walletAddress}`);
+//   }
+//   else {
+//     console.log("No wallet");
+//     window.web3 = new Web3(window.ethereum);
+//     const account = web3.eth.accounts;
+//   }
+//   Change();
+// }
+// window.onload = function () {
+//   Change();
+// }
+// function Change() {
+//   let wall = document.getElementById("connectWall").firstElementChild;
+//   if (ethereum.selectedAddress != null) {
+//     wall.innerHTML = "Connected";
+//     document.getElementById("connectWall").firstElementChild.style.cursor = "not-allowed";
+//   }
+//   else {
+//     wall.innerHTML = "Connect Wallet";
+//     wall.style.cursor = "pointer";
+//   }
+// }
