44e273249e6642b34e154d9bb332a13f7aed74d6
Davit Berulava <48175961+davit2003@users.noreply.github.com>
2022-09-13 00:23:07 +0300

    New Wallet Connecting

--- js/connectmeta.js
+++ js/connectmeta.js
@@ -1,32 +1,32 @@
-// async function connect() {
-//   if (window.ethereum) {
-//     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-//     // const account = accounts[0];
-//     //Get the current MetaMask selected/active wallet
-//     const walletAddress = ethereum.selectedAddress;
-//     // console.log(`Wallet: ${walletAddress}`);
-//   }
-//   else {
-//     console.log("No wallet");
-//     window.web3 = new Web3(window.ethereum);
-//     const account = web3.eth.accounts;
-//   }
-//   Change();
-// }
-// window.onload = function () {
-//   Change();
-// }
-// function Change() {
-//   let wall = document.getElementById("connectWall").firstElementChild;
-//   if (ethereum.selectedAddress != null) {
-//     wall.innerHTML = "Connected";
-//     document.getElementById("connectWall").firstElementChild.style.cursor = "not-allowed";
-//   }
-//   else {
-//     wall.innerHTML = "Connect Wallet";
-//     wall.style.cursor = "pointer";
-//   }
-// }
+async function connect() {
+  if (window.ethereum) {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    // const account = accounts[0];
+    //Get the current MetaMask selected/active wallet
+    const walletAddress = ethereum.selectedAddress;
+    // console.log(`Wallet: ${walletAddress}`);
+  }
+  else {
+    console.log("No wallet");
+    window.web3 = new Web3(window.ethereum);
+    const account = web3.eth.accounts;
+  }
+  // Change();  
+}
+window.onload = function () {
+  Change();
+}
+function Change() {
+  let wall = document.getElementById("connectWall").firstElementChild;
+  if (ethereum.selectedAddress != null) {
+    wall.innerHTML = "Connected";
+    document.getElementById("connectWall").firstElementChild.style.cursor = "not-allowed";
+  }
+  else {
+    wall.innerHTML = "Connect Wallet";
+    wall.style.cursor = "pointer";
+  }
+}
 
 function openWallet() {
   document.querySelector("#WalletModal").classList.add("show");
@@ -36,3 +36,12 @@ function closeWallet() {
   document.querySelector("#WalletModal").classList.add("show");
   document.querySelector("#WalletModal").style.display = "none";
 }
+
+connector.on("connect", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Get provided accounts and chainId
+  const { accounts, chainId } = payload.params[0];
+});
