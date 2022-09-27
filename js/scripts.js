3ec69cf68c1e1166028d599bcb5d8fdf3a62c225
Davit Berulava <48175961+davit2003@users.noreply.github.com>
2022-09-28 02:02:50 +0300

    Loading Page Update

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,30 @@ let loader = document.getElementById("preloader");
 
 window.addEventListener('load', function(){
   loader.style.display = "none";
-})
+  document.body.style.overflow = 'hidden';
+  threedot();
+  setInterval(() => {
+    threedot();
+  }, 4000);
+});
+
+let dot1 = document.getElementsByClassName("dot1")[0];
+let dot2 = document.getElementsByClassName("dot2")[0];
+let dot3 = document.getElementsByClassName("dot3")[0];
+function threedot(){
+  dot1.style.display = "none";
+  dot2.style.display = "none";
+  dot3.style.display = "none";
+  setTimeout(() => {
+    dot1.style.display = "inline";
+  }, 1000);
+  setTimeout(() => {
+    dot2.style.display = "inline";
+  }, 2000);
+  setTimeout(() => {
+    dot3.style.display = "inline";
+  }, 3000);
+}
 
 
 var swiper1 = new Swiper(".mySwiper1", {
