97e6f6175e8e92ec9bad67444a20c49a1d1ad6e0
Davit Berulava <48175961+davit2003@users.noreply.github.com>
2022-09-30 18:50:45 +0300

    Opacity

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,11 @@
 let loader = document.getElementById("preloader");
 window.addEventListener('load', function(){
-  loader.style.display = "none";
-  document.body.style.overflow = 'hidden';
-  document.body.style.overflowY = 'scroll';
+  setTimeout(() => {
+    loader.style.opacity = 0;
+  }, "500")
+  setTimeout(() => {
+    loader.style.display = "none"
+    document.body.style.overflow = 'hidden';
+    document.body.style.overflowY = 'scroll';
+  }, "1000")
 });
